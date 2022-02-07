65670e2caabdcef6db2813a78e5c8ff8ada8210b
SaucySalamander <26693281+SaucySalamander@users.noreply.github.com>
2022-02-07 01:05:56 -0600

    Initial commit

---
+++ emulation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#cargo build
+cargo clean
+cargo build -Z build-std="core","compiler_builtins","alloc" --target x86_64-unknown-uefi
+
+mkdir -p ./target/x86_64-unknown-uefi/debug/esp/EFI/Boot/
+cp ./target/x86_64-unknown-uefi/debug/bootloader.efi ./target/x86_64-unknown-uefi/debug/esp/EFI/Boot/BootX64.efi
+
+qemu-system-x86_64 -enable-kvm -drive if=pflash,format=raw,unit=0,readonly=on,file=/usr/share/ovmf/x64/OVMF.fd -drive if=pflash,format=raw,unit=1,readonly=on,file=/usr/share/ovmf/x64/OVMF_VARS.fd -drive format=raw,file=fat:rw:target/x86_64-unknown-uefi/debug/esp/
